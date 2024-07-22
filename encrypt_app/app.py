946ea609e8cc83c3030cdbdfed5eac2ce7a43d51
MichMatrix <michel.a.s401@gmail.com>
2024-07-21 23:57:12 -0300

    feat: create function to decrypt using AES key

--- encrypt_app/app.py
+++ encrypt_app/app.py
@@ -13,6 +13,17 @@ def encrypt_password(password):
     ciphertext, tag = cipher.encrypt_and_digest(password.encode('utf-8'))
     return base64.b64encode(nonce + ciphertext).decode('utf-8'), key
 
+# Função para desencriptografar a senha
+def decrypt_password(encrypted_password, key_hex):
+    key = bytes.fromhex(key_hex)
+    encrypted_data = base64.b64decode(encrypted_password)
+    nonce = encrypted_data[:16]
+    ciphertext = encrypted_data[16:]
+    cipher = AES.new(key, AES.MODE_EAX, nonce=nonce)
+    password = cipher.decrypt(ciphertext).decode('utf-8')
+    return password
+
+
 
 
 if __name__ == '__main__':
